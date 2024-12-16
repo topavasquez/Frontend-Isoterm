ae30787b3cd753778b8d133149c95912a9df7a51
topavasquez <tomas.vasquez@alumnos.ubiobio.cl>
2024-12-16 17:32:27 -0300

    pichula

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import Link from "next/link";
+
 
 const Header = () => {
   const navbarItems = [
@@ -11,31 +11,48 @@ const Header = () => {
   ];
 
   return (
-    <header className="w-full my-6 px-2 sm:px-8">
-      <div className="flex sm:px-6 sm:px-14 sm:items-center sm:gap-4">
-        <h2 className="flex-1">
-          <Link
-            href="/"
-            className="flex sm:flex-row items-center gap-2 sm:gap-6"
-          >
-            <Image src="/logo.png" alt="Logo" width={50} height={50} />
-            <div>
-              <span className="font-bold text-2xl sm:text-1xl text-blue-500">
-                Isoterm
-              </span>
-              <span className="font-bold text-2xl sm:text-1xl text-white">
-                Chile
-              </span>
-            </div>
-          </Link>
-        </h2>
-        <ul className="flex flex-col items-center md:flex-row gap-10 md:gap-4 min-[900px]:gap-5 lg:gap-12 justify-end text-sm md:text-[15px] leading-[22px]">
-          {navbarItems.map(({ ref, label }) => (
-            <li key={ref} className="relative">
-              <Link href={ref}>{label}</Link>
-            </li>
-          ))}
-        </ul>
+    <header className="relative w-full h-[50vh]">
+
+      <div className="absolute inset-0">
+        <Image
+          src="/banner.jpg" 
+          alt="Banner Background"
+          layout="fill"
+          objectFit="cover"
+          className=""
+        />
+
+        <div className="absolute inset-0 bg-black opacity-50"></div>
+      </div>
+
+ 
+      <div className="relative z-10 w-full h-full flex flex-col px-4 sm:px-8">
+        <div className="flex items-center justify-between sm:px-6 sm:gap-4 mt-4">
+
+          <h2>
+            <a href="/" className="flex sm:flex-row items-center gap-2 sm:gap-6">
+              <Image src="/logo1.png" alt="Logo" width={100} height={100} />
+              <div>
+                <span className="font-bold text-2xl sm:text-3xl text-blue-300">
+                  Isoterm
+                </span>
+                <span className="font-bold text-2xl sm:text-3xl text-white">
+                  Chile
+                </span>
+              </div>
+            </a>
+          </h2>
+
+          <ul className="flex flex-col items-center md:flex-row gap-10 md:gap-4 min-[900px]:gap-5 lg:gap-12 justify-end text-sm md:text-[15px] leading-[22px] text-white">
+            {navbarItems.map(({ ref, label }) => (
+              <li key={ref} className="relative">
+                <a href={ref} className="hover:text-blue-400 transition-colors">
+                  {label}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </header>
   );
