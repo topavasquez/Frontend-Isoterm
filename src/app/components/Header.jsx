4bbdaea71682cfe5ab3c86f3de241fd2fe919e58
iPipee <felipe2002ft@gmail.com>
2024-12-16 14:10:56 -0300

    mr toupa error

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header>
+            <div>
+                <h2>Isoterm Chile</h2>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
+
+
