63540279e7b4e6585341605138c7d1d59c66accf
iPipee <felipe2002ft@gmail.com>
2024-12-20 19:09:10 -0300

    Add href y estilo politica

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -29,7 +29,7 @@ const Footer = () => {
                   <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Privacidad</h2>
                   <ul className="text-gray-500 dark:text-gray-400 font-medium">
                       <li className="mb-4">
-                          <a href="#" className="hover:underline">Politica de Privacidad</a>
+                          <a href="/politica" className="hover:underline">Politica de Privacidad</a>
                       </li>
                       <li>
                           <a href="#" className="hover:underline">Terminos &amp; Condiciones</a>
